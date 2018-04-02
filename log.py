a4f8191bf91c8c1fd4c6d4b760f4a3a7f87378e9
Thiago <thiago.engh@gmail.com>
2018-04-02 07:19:37 -0300

    commit with pip8 format sintax

--- log.py
+++ log.py
@@ -17,42 +17,41 @@ query3 = "select * from error_log_view where \"Percent Error\" > 1"
 
 
 def get_query1():
-	db = psycopg2.connect(database=DBNAME)
-	c = db.cursor()
-	c.execute(query1)
-	results = c.fetchall()
-	db.close()
-	print ("query1")
-	for result in results:
-		print ('\t' + str(result[0]) + ' ---> ' + str(result[1]) + ' views')
-	return
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    c.execute(query1)
+    results = c.fetchall()
+    db.close()
+    print("query1")
+    for result in results:
+        print('\t' + str(result[0]) + ' ---> ' + str(result[1]) + ' views')
+    return
+
 
 def get_query2():
-	db = psycopg2.connect(database=DBNAME)
-	c = db.cursor()
-	c.execute(query2)
-	results = c.fetchall()
-	db.close()
-	print ("query2")
-	for result in results:
-		print ('\t' + str(result[0]) + ' ---> ' + str(result[1]) + ' views')
-	return
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    c.execute(query2)
+    results = c.fetchall()
+    db.close()
+    print("query2")
+    for result in results:
+        print('\t' + str(result[0]) + ' ---> ' + str(result[1]) + ' views')
+    return
+
 
 def get_query3():
-	db = psycopg2.connect(database=DBNAME)
-	c = db.cursor()
-	c.execute(query3)
-	results = c.fetchall()
-	db.close()
-	print ("query3")
-	for result in results:
-		print ('\t' + str(result[0]) + ' ---> ' + str(result[1]) + ' %')
-	return results
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    c.execute(query3)
+    results = c.fetchall()
+    db.close()
+    print("query3")
+    for result in results:
+        print('\t' + str(result[0]) + ' ---> ' + str(result[1]) + ' %')
+    return results
 
 
 get_query1()
 get_query2()
 get_query3()
-
-
-
